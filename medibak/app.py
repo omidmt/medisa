44a340897b97821b26d7d5e809d75e0afce34dc4
Omid <>
2023-08-17 07:56:34 +1000

    Add podcast episode list api.

--- medibak/app.py
+++ medibak/app.py
@@ -28,6 +28,59 @@ def get_podcasts():
     except Exception as e:
         return str(e), 500
 
+@app.route('/podcast/<int:podcast_id>', methods=['GET'])
+def get_podcast_details(podcast_id):
+    try:
+        # Connect to the SQLite database
+        conn = sqlite3.connect(get_config().db_file)
+        cursor = conn.cursor()
+
+        # Query to get the podcast details
+        cursor.execute("SELECT * FROM podcasts WHERE id = ?", (podcast_id,))
+        podcast = cursor.fetchone()
+        if podcast is None:
+            return "Podcast not found", 404
+
+        podcast_details = {
+            # "id": podcast[0],
+            # "user_id": podcast[1],
+            "name": podcast[2],
+            # "xml_url": podcast[3],
+            "html_url": podcast[4],
+            "image_url": podcast[5],
+            "title": podcast[6],
+            "description": podcast[7],
+            # "language": podcast[8],
+            # "timestamp": podcast[9]
+        }
+
+        # Query to get the episodes of the podcast
+        cursor.execute("SELECT * FROM episodes WHERE podcast_id = ?", (podcast_id,))
+        episodes = cursor.fetchall()
+
+        # Convert the result into a list of dictionaries
+        episodes_list = [{
+            "id": e[0],
+            # "podcast_id": e[1],
+            "url": e[2],
+            # "file_name": e[3],
+            "title": e[4],
+            "description": e[5],
+            "pub_date": e[6],
+            "link": e[7],
+            "image_url": e[8],
+            "downloaded": e[9],
+            # "timestamp": e[10],
+            # "update_time": e[11]
+        } for e in episodes]
+
+        # Close the connection
+        conn.close()
+
+        return jsonify({"podcast": podcast_details, "episodes": episodes_list})
+    except Exception as e:
+        return str(e), 500
+
 
 if __name__ == '__main__':
     app.run(port=5000)
